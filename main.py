a116f067eea3452c1dae5f77d1662d7291d3a8b5
v-amna <amnamampad@gmail.com>
2026-02-09 11:57:59 +0100

    Implement student data management with JSON support and add initial student records

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
 from student import Student, StudentManager
 
 
-
 studentmanager=StudentManager()
-studentmanager.create_student()
+studentmanager.load('students.json')
+# studentmanager.add_student_from_dict({"id":1, "name": "John Doe","gender": "Male","english_score": 85,"math_score": 90,"science_score": 80,"art_score": 34})
+# studentmanager.add_student_from_dict({"id":2, "name": "Chakka","gender": "Male","english_score": 85,"math_score": 90,"science_score": 80,"art_score": 67})
+# studentmanager.add_student_from_dict({"id":3, "name": "Doe D","gender": "Male","english_score": 34,"math_score": 43,"science_score": 56,"art_score": 9})
+# studentmanager.add_student_from_dict({"id":4, "name": "Ela F","gender": "Female","english_score": 76,"math_score": 76,"science_score": 89,"art_score": 45})
 studentmanager.list_students()
-studentmanager.update_student(1)
-studentmanager.list_students()
-studentmanager.delete_student(1)
-studentmanager.list_students()
+print("="*50)
+# studentmanager.create_student()
+# studentmanager.update_student(1)
+# studentmanager.list_students()
+# studentmanager.delete_student(1)
+# studentmanager.list_students()
 
-sId=input("Enter student ID to search for: ")
-studentmanager.search_student_by_id(int(sId))
+# sId=input("Enter student ID to search for: ")
+# studentmanager.search_student_by_id(int(sId))
--- student.py
+++ student.py
@@ -1,25 +1,28 @@
 from dataclasses import dataclass
+import json
+from pathlib import Path
+
 
 # This class represents a student with their name, gender, and scores in various subjects.
 # The __str__ method is overridden to provide a readable string representation of the student object when printed.
 # dataclass is used to automatically generate special methods like __init__ and __repr__, making it easier to create and manage student objects.
 @dataclass
 class Student:
+    id:int
     name:str
     gender:str
-    english_score:int =0
-    math_score:int =0
-    science_score:int =0
-    art_score:int =0
+    english_score:int = 0
+    math_score:int = 0
+    science_score:int = 0
+    art_score:int = 0
     
     def __str__(self):
-        return f"Name: {self.name}| Gender: {self.gender}| total score: {self.total_score()} | English Score: {self.english_score}| Math Score: {self.math_score}| Science Score: {self.science_score}| Art Score: {self.art_score}"
-
+        return f"Id: {self.id} | Name: {self.name} | Gender: {self.gender} | English Score: {self.english_score} | Math Score: {self.math_score} | Science Score: {self.science_score} | Art Score: {self.art_score} | Total score: {self.total_score()}"
 
     # Method to calculate total score of the student by summing up the scores in all subjects.
     def total_score(self) -> int:
-        
         return self.english_score + self.math_score + self.science_score + self.art_score
+    
 # This class is a manager for student objects, to add,update,delete,view,search and filter students.
 class StudentManager:
 
@@ -29,10 +32,13 @@ class StudentManager:
     # varible to keep track of the next student Id.
     next_id: int = 1
 
+    file_path: str = "students.json"
+
 
     # Method to add a new student and returns the assigned student Id.
     def add_student(self, student: Student) -> int:
         student_id = self.next_id
+        student.id = student_id  # Assign the generated ID to the student object
         self.students[student_id] = student
         self.next_id += 1
         return student_id
@@ -56,9 +62,10 @@ class StudentManager:
             "art_score": art_score
         }
     
-    # Method to create student object from floatgiven dict
+    # Method to create student object from given dictionary
     def from_dict(self, data: dict) -> Student:
         return Student(
+            id=data["id"] if "id" in data else 0,  # ID will be assigned when the student is added to the manager
             name=data["name"],
             gender=data["gender"],
             english_score=int(data["english_score"]),
@@ -67,6 +74,12 @@ class StudentManager:
             art_score=int(data["art_score"])
         )
     
+    # Method to add a student from given dictionary, 
+    # it creates a student object and adds it to the manager.
+    def add_student_from_dict(self, data: dict) -> int:
+        student = self.from_dict(data)
+        return self.add_student(student)
+
     # read student details from the user and create a new student object, then add it to the manager.
     def create_student(self):
         print("Enter details for the new student:\n")
@@ -74,6 +87,7 @@ class StudentManager:
             self.read_student_details())
         
         student_id = self.add_student(student)
+        self._save()  # Save the updated students data to the JSON file after adding a new student
         print(f"Student added with ID: {student_id}")
 
     # Method to list all students 
@@ -81,8 +95,8 @@ class StudentManager:
         if not self.students:
             print("No students exist.")
             return
-        for student_id, student in self.students.items():
-            print(f"ID: {student_id} | {student}")
+        for student in self.students.values():
+            print(f"{student}")
     
     # Method to Update by student Id
     def update_student(self, student_id: int):
@@ -90,7 +104,6 @@ class StudentManager:
         if student_id not in self.students:
             print(f"Student with ID {student_id} does not exist.")
             return
-        float
         updated_data = self.read_student_details()
         student = self.students[student_id]
     
@@ -102,7 +115,7 @@ class StudentManager:
         student.science_score = int(updated_data["science_score"]) if  updated_data["science_score"] else student.science_score
         student.art_score = int(updated_data["art_score"]) if  updated_data["art_score"] else student.art_score
         print(f"Student with ID {student_id} has been updated.")
-
+        self._save()  # Save the updated students data to the JSON file after updating a student
 
     # Method to delete a student by their Id
     def delete_student(self, student_id: int):
@@ -112,6 +125,7 @@ class StudentManager:
             if student_id in self.students:
                 del self.students[student_id]
                 print(f"Student with ID {student_id} has been deleted.")
+                self._save()  # Save the updated students data to the JSON file after deleting a student
             else:
         
                 print(f"Student with ID {student_id} does not exist.")
@@ -119,7 +133,6 @@ class StudentManager:
             print("Deletion process canceled.")
 
     #Method to search for students by id 
-
     def search_student_by_id(self, student_id: int):
         """Searches for a student by their ID."""
    
@@ -127,5 +140,31 @@ class StudentManager:
             print(f"ID: {student_id} | {self.students[student_id]}")
         else:
             print(f"Student with ID {student_id} does not exist.")
-   
+
+    # Method to create a json dump of students data
+    def _save(self):
+        """Saves the students data to a JSON file."""
+        with open(self.file_path, 'w') as f:
+            print(f"Saving students data to {self.file_path}...")       
+            json.dump([student.__dict__ for student in self.students.values()] ,fp=f, indent=4)
+            return
+        print(f"Students data file save failed to save to {self.file_path}.")
+
+    # Method to load students data from a json file
+    def load(self,file_path: str = None):
+        """Loads the students data from a JSON file."""
+        self.file_path = file_path if file_path else self.file_path
+
+        if not Path(self.file_path).exists():
+            print(f"No existing data file found at {self.file_path}. Starting with an empty student manager.")
+            return
+        with open(self.file_path, 'r') as f:
+            print(f"Loading students data from {self.file_path}...")
+            students_data = json.load(f)
+            for student_data in students_data:
+                student = self.from_dict(student_data)
+                self.students[student.id] = student
+                self.next_id = max(self.next_id, student.id + 1)  # Ensure next_id is always greater than the highest existing ID
+
+    #Method to filter students by Marks ,whose got distINCTION, FIRST CLASS, SECOND CLASS, THIRD CLASS and FAIL    
     
